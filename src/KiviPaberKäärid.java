4cc1721f45cf14cb80e4283151f31f96f888627c
karl-martin-karno <karl.martin.karno@gmail.com>
2020-03-26 17:28:12 +0200

    Lisasin randomi.

--- src/KiviPaberKäärid.java
+++ src/KiviPaberKäärid.java
@@ -2,14 +2,14 @@ import java.util.Random;
 
 public class KiviPaberKäärid {
 
-    public static void arvutiValik(){ //Lihtsalt see paika, et tuleks random valik. Peaks salvestama
+    public static void arvutiValik() { //Lihtsalt see paika, et tuleks random valik. Peaks salvestama
         //arvatavasti Stringi, et saaks võrrelda meie valikud ja arvutit.
         double suvalinearv = Math.random();
-        if (suvalinearv<0.33)
+        if (suvalinearv < 0.33)
             System.out.println("Kivi");
-        if (suvalinearv>0.33&& suvalinearv<0.66)
+        if (suvalinearv > 0.33 && suvalinearv < 0.66)
             System.out.println("Käärid");
-        if (suvalinearv>0.66)
+        if (suvalinearv > 0.66)
             System.out.println("Paber");
     }
 }
