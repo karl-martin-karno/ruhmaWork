cf178d6b86578202995d63df1b7055c9aac0a04c
karl-martin-karno <karl.martin.karno@gmail.com>
2020-03-30 13:03:28 +0300

    Meetodid : arvuti ja inimese valik valmis.

--- src/KiviPaberKäärid.java
+++ src/KiviPaberKäärid.java
@@ -3,81 +3,42 @@ import java.util.Scanner;
 
 public class KiviPaberKäärid {
 
-    public static String meieOtsus;
-    public static String arvuti;
 
-    public static void arvutiValik() { //Lihtsalt see paika, et tuleks random valik. Peaks salvestama
-        //arvatavasti Stringi, et saaks võrrelda meie valikud ja arvutit.
+    public static String arvutiValik() {
+        String arvutiOtsus;
 
         double suvalinearv = Math.random();
 
         if (suvalinearv < 0.33) {
 
-            arvuti = "kivi";
-            System.out.println(arvuti);
+            arvutiOtsus = "kivi";
+            System.out.println(arvutiOtsus);
+
+        } else if (suvalinearv > 0.33 && suvalinearv < 0.66) {
+
+            arvutiOtsus = "käärid";
+            System.out.println(arvutiOtsus);
+        } else {
+
+            arvutiOtsus = "paber";
+            System.out.println(arvutiOtsus);
         }
-
-        if (suvalinearv > 0.33 && suvalinearv < 0.66) {
-
-            arvuti = "käärid";
-            System.out.println(arvuti);
-        }
-
-        if (suvalinearv > 0.66) {
-
-            arvuti = "paber";
-            System.out.println(arvuti);
-        }
+        return arvutiOtsus;
 
 
     }
 
-    public static void meievalik() {
+    public static String inimeseValik() {
         System.out.println("Tee oma valik: kivi, paber või käärid?");
         Scanner in = new Scanner(System.in);
         String meieOtsus = in.nextLine();
+        return meieOtsus;
 
-        }
-
-        public static void tulemus(){
-        if (meieOtsus.equals("kivi")
-                &&arvuti.equals("kivi")){
-            System.out.println("Viik");
-        }
-         if (meieOtsus.equals("paber")
-                 &&arvuti.equals("paber")){
-             System.out.println("Viik");
-         }
-         if (meieOtsus.equals("käärid")
-                 &&arvuti.equals("käärid")){
-             System.out.println("Viik");
-         }
-         if (meieOtsus.equals("kivi")
-                 &&arvuti.equals("paber")){
-             System.out.println("Arvuti võit");
-         }
-         if (meieOtsus.equals("kivi")
-                 &&arvuti.equals("käärid")){
-             System.out.println("Sinu võit");
-         }
-         if (meieOtsus.equals("paber")
-                 &&arvuti.equals("käärid")){
-             System.out.println("Arvuti võit");
-         }
-         if (meieOtsus.equals("paber")
-                 &&arvuti.equals("kivi")){
-             System.out.println("Sinu võit");
-         }
-         if (meieOtsus.equals("käärid")
-                 &&arvuti.equals("paber")){
-             System.out.println("Sinu võit");
-         }
-         if (meieOtsus.equals("käärid")
-                 &&arvuti.equals("kivi")){
-             System.out.println("Arvuti võit");
-         }
-
-     }
     }
+
+
+
+
+}
 
 
